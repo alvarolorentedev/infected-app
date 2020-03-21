12c317aedfe9af6f45cfee23c7f6f437cb478af4
Kevin Mas Ruiz <masruizkevin@gmail.com>
2020-03-21 20:56:19 +0100

    Post mutation through graphql

--- src/services/game.js
+++ src/services/game.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const createGameQuery = `mutation { createGame() { id } }`
+
 export const createGame = async () => {
-    return (await axios.post("/graphql")).data
+    return (await axios.post("/graphql", createGameQuery)).data
 }
--- test/services/game.test.js
+++ test/services/game.test.js
@@ -1,15 +1,17 @@
 import * as GameService from '../../src/services/game'
+import fs from 'fs'
 import { random } from 'faker'
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter'
 
 describe('Game Service', () => {
+    const createGameGraphqlQuery = fs.readFileSync("test/services/createGame.sample.gql", "ascii");
 
     describe('action to create a new game', () => {
         it('should call the backend to create a new game', async () => {
             const mock = new MockAdapter(axios)
             const gameId = random.uuid()
-            mock.onPost("/graphql").replyOnce(200, { id: gameId })
+            mock.onPost("/graphql", createGameGraphqlQuery).replyOnce(200, { id: gameId })
 
             const game = await GameService.createGame();
 
