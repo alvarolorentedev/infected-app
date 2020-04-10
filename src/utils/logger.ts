16a1f8ece6846a60070f72121d7e9b7c308d75f4
kanekotic <me@kanekotic.com>
2020-04-10 19:24:04 +0200

    added again loggly

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,6 @@
 import * as winston from 'winston';
+import { Loggly } from 'winston-loggly-bulk';
+import ENV from './constants';
 
 const logger = winston.createLogger({
   level: 'info',
@@ -10,5 +12,17 @@ logger.add(
     format: winston.format.simple(),
   })
 );
+
+if (ENV.ENVIRONMENT === 'production') {
+  logger.add(
+    new Loggly({
+      subdomain: ENV.LOGS_SUBDOMAIN,
+      inputToken: ENV.LOGS_INPUT_TOKEN,
+      tags: ['infecteed', 'app'],
+      isBulk: true,
+      stripColors: true,
+    })
+  );
+}
 
 export default logger;
