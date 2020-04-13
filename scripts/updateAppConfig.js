cbc0c0e95a0940040455892286cf5f0d77246f58
kanekotic <me@kanekotic.com>
2020-04-13 17:15:26 +0200

    pass correct variable in github build

--- scripts/updateAppConfig.js
+++ scripts/updateAppConfig.js
@@ -11,7 +11,5 @@ file.expo.version = `${currentVersion[0]}.${currentVersion[1]}.${process.env.GIT
 file.expo.extra.prod.SERVER_URL = process.env.SERVER_URL
 file.expo.extra.prod.SERVER_USERNAME = process.env.SERVER_USERNAME
 file.expo.extra.prod.SERVER_PASSWORD = process.env.SERVER_PASSWORD
-file.expo.extra.prod.LOGS_SUBDOMAIN = process.env.LOGS_SUBDOMAIN
-file.expo.extra.prod.LOGS_INPUT_TOKEN = process.env.LOGS_INPUT_TOKEN
 
 fs.writeFileSync('app.json', JSON.stringify(file))
